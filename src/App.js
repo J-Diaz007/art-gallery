7ae28d2fcbda0a26a5ac185dd06a156075566848
JDiaz <jdiaz_007@outlook.com>
2023-09-24 22:03:41 -0700

    rendered gallery and buttonbar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ButtonBar from './Components/ButtonBar';
+import Gallery from './Components/Gallery'
+
+function App() {
+  return (
+    <div className="App">
+    <Gallery />
+    <ButtonBar />
+    </div>
+  );
+}
+
+export default App;
